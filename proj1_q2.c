43af3bc01a90cbb812da3259e02df59734389632
Bryanong9440 <bongzenh@terpmail.umd.edu>
2024-09-26 11:29:11 -0400

    Update proj1_q2.c

--- proj1_q2.c
+++ proj1_q2.c
@@ -6,20 +6,20 @@ int main()
     scanf("%i", &a);
     int b = 2;
     if(a <= 1){
-        printf("%i is not a prime number\n", a);
+        printf("%i is not a prime number.\n", a);
     }
     if(a == 2){
-        printf("%i is a prime number\n", a);
+        printf("%i is a prime number.\n", a);
     }
     while(a % b != 0 && a > 2){
         if (a > b){
         b++;    
         }
         if (a == b){
-            printf("%i is a prime number\n", a);
+            printf("%i is a prime number.\n", a);
         }
     }
     if(a > b){
-        printf("%i is not a prime number\n", a);
+        printf("%i is not a prime number.\n", a);
     }
 }
