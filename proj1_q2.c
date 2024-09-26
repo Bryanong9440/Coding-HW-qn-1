0af6b36d227f37f15c138937407e7dbd5d55a151
Bryanong9440 <bongzenh@terpmail.umd.edu>
2024-09-26 11:28:28 -0400

    Update proj1_q2.c

--- proj1_q2.c
+++ proj1_q2.c
@@ -2,24 +2,24 @@
 int main()
 {
     int a;
-    printf("Please enter an integer: ");
+    printf("Please enter an integer: \n");
     scanf("%i", &a);
     int b = 2;
     if(a <= 1){
-        printf("%i is not a prime number", a);
+        printf("%i is not a prime number\n", a);
     }
     if(a == 2){
-        printf("%i is a prime number", a);
+        printf("%i is a prime number\n", a);
     }
     while(a % b != 0 && a > 2){
         if (a > b){
         b++;    
         }
         if (a == b){
-            printf("%i is a prime number", a);
+            printf("%i is a prime number\n", a);
         }
     }
     if(a > b){
-        printf("%i is not a prime number", a);
+        printf("%i is not a prime number\n", a);
     }
 }
